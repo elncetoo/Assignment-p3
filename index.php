2ec4429b58ac2a436b57c3b979686bb207eb80f7
elle <svilena_koleva@yahoo.com>
2016-10-07 20:31:32 +0200

    setting up the page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Assignment - part #3</title>
+</head>
+
+<body>
+<!-- Site navigation menu -->
+<ul class="navbar">
+  <li><a href="index.php">Home page</a>
+  <li><a href="projects.php">Musings</a>
+  <li><a href="clients.php">My town</a>
+  </ul>
+
+
+</body>
+</html>
